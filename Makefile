2ff987e77c4a8d7a8c49b791e66d5b829128d504
Farrah Lin <flin27@jhu.edu>
2017-05-03 19:30:51 -0400

    compiling unittest

--- Makefile
+++ Makefile
@@ -1,14 +1,17 @@
 CXX = g++
 CXXFLAGS = -Wall -Wextra -pedantic -std=c++11 -g
 
+all: chess unittest
+
 chess: Chess.o Board.o main.o
 	$(CXX) $(CXXFLAGS) Chess.o Board.o main.o -o chess
-unittest: test_chess.o Chess.o
+unittest: test_chess.o Chess.o Board.o
+	$(CXX) $(CXXFLAGS) Chess.o test_chess.o Board.o -o unittest
 
 Board.o: Game.h
 Chess.o: Game.h Chess.h Prompts.h
 main.o: Game.h Chess.h Prompts.h main.cpp Chess.cpp
-test_chess.o: test_chess.cpp Chess.cpp
+test_chess.o: test_chess.cpp Chess.cpp Chess.h Game.h Board.cpp
 
 clean:
 	rm *.o chess
--- test_chess.cpp
+++ test_chess.cpp
@@ -1,6 +1,5 @@
 #include <cassert>
 #include "Chess.h"
-#include "Terminal.h"
 #include "Game.h"
 
 
@@ -10,26 +9,31 @@ test_chess
 */
 
 class test_chess {
-  ChessGame new;
 
 public:
-  test_chess () {
-    new = ChessGame();
-    new.setupBoard();
-    m_turn=1;
+  ChessGame c1;
+  test_chess() {
+    cout<<"new"<<endl;
+    c1 = ChessGame();
+    cout<<"c1"<<endl;
+    c1.setupBoard();
+    cout<<"setuo"<<endl;
+    c1.m_turn = 1;
+    cout<<"turn"<<endl;
   }
 
+  void newGameIsNotOver() {
+    assert(!c1.gameOver());
+  }
 
-
-
-  
+ 
 
 };
 
 int main() {
+  cout<<"begining"<<endl;
   test_chess t;
-
-
+  t.newGameIsNotOver();
   
   return 0;
 }
