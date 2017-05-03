c8d74474551c13e6baca63627e7b4be915e2019d
Farrah Lin <flin27@jhu.edu>
2017-05-03 19:43:30 -0400

    unittest won't set up board :(

--- test_chess.cpp
+++ test_chess.cpp
@@ -12,28 +12,38 @@ class test_chess {
 
 public:
   ChessGame c1;
+ 
   test_chess() {
     cout<<"new"<<endl;
     c1 = ChessGame();
-    cout<<"c1"<<endl;
-    c1.setupBoard();
+    //cout<<"c1"<<endl;
+    //c1.setupBoard();
     cout<<"setuo"<<endl;
     c1.m_turn = 1;
     cout<<"turn"<<endl;
   }
 
   void newGameIsNotOver() {
+    //cout<<c1.gameOver()<<endl;
     assert(!c1.gameOver());
+    cout<<"passed newGameIsNotOver"<<endl;
   }
 
+  void validPawn() {
+    Position start(0,6);
+    Position end(0,2);
+    int i = c1.makeMove(start, end);
+    cout<<"passed" <<i<<endl;
+  }
  
 
 };
 
 int main() {
-  cout<<"begining"<<endl;
   test_chess t;
   t.newGameIsNotOver();
+  t.validPawn();
   
+  cout<<"about to return"<<endl;
   return 0;
 }
