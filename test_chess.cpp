a470b5a5190c30682c375176e7d9487b7dbd1bcc
Farrah Lin <flin27@jhu.edu>
2017-05-03 20:54:36 -0400

    working on unittest

--- test_chess.cpp
+++ test_chess.cpp
@@ -12,38 +12,89 @@ class test_chess {
 
 public:
   ChessGame c1;
+  //Pawn *  p = new Pawn(Player(WHITE), 0);
+  
  
   test_chess() {
-    cout<<"new"<<endl;
-    //c1 = ChessGame();
-    //cout<<"c1"<<endl;
     c1.setupBoard();
-    cout<<"setuo"<<endl;
     c1.m_turn = 1;
-    cout<<"turn"<< endl;
   }
+
+  /*  ChessGame* before(){
+    cout<<"before"<<endl;
+    ChessGame * c2 = new ChessGame();
+    //c2.setupBoard();
+    //c1 =c2;
+    c2->setupBoard();
+    c2->m_turn = 1;
+    return c2;
+    }*/
 
   void newGameIsNotOver() {
-    //cout<<c1.gameOver()<<endl;
     assert(!c1.gameOver());
-    cout<<"passed newGameIsNotOver"<<endl;
+    cout<<"passed newGameIsNotOver"<<endl<<endl;
+  }
+  
+  void invalidDiagonalURPawn() {
+    Position start(1,1);
+    Position end(3,2);
+    int i = c1.makeMove(start, end);
+    assert(i == MOVE_ERROR_ILLEGAL);
+    c1.printMoveMessages(i);
+    cout<<"passed invalidDiagonalURPawn"<<endl<<endl;
   }
 
-  void validPawn() {
-    Position start(0,1);
+  
+  void invalidDiagonalULPawn() {
+    Position start(1,1);
     Position end(0,2);
     int i = c1.makeMove(start, end);
-    cout<<"passed" <<i<<endl;
+    assert(i == MOVE_ERROR_ILLEGAL);
+    c1.printMoveMessages(i);
+    cout<<"passed invalidDiagonalULPawn\n"<<endl;
   }
+
+  
+  void invalidDownPawn() {
+    Position start(1,1);
+    Position end(0,1);
+    int i = c1.makeMove(start, end);
+    assert(i < 0);
+    c1.printMoveMessages(i);
+    cout<<"passed invalidDownPawn\n"<<endl;
+  }
+
+  void invalidNoPiece(){
+    Position start(0,6);
+    Position end(0,5);
+    int i = c1.makeMove(start, end);
+    assert(i < 0);
+    cout<<"passed invalidNoPiece\n"<<endl;
+  }
+
+  void offBoard(){
+    Position start(-1,-1);
+    Position end(9,9);
+    int i = c1.makeMove(start, end);
+    assert(i < 0);
+    cout<<"passed offBoard\n"<<endl;
+  }
+
+  
+  
  
 
 };
 
 int main() {
   test_chess t;
-  t.newGameIsNotOver();
-  t.validPawn();
   
+  t.newGameIsNotOver();
+  t.invalidDiagonalURPawn();
+  t.invalidDiagonalULPawn();
+  t.invalidDownPawn();
+  t.invalidNoPiece();
+  t.offBoard();
   cout<<"about to return"<<endl;
   return 0;
 }
